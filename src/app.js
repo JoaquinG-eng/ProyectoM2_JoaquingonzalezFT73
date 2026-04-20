a7b8304c09788078d2c2c13ae9450b039bf8a7f1
Joaquin <joamengancho@gmail.com>
2026-04-20 11:25:45 -0300

    implementacion de cors

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ import YAML from "yamljs";
 import authorsRoutes from "./routes/Authors.Routes.js";
 import postsRoutes from "./routes/Posts.Routes.js";
 import { errorHandler } from "./middlewares/Error.Middlewares.js";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use("/api/authors", authorsRoutes);
