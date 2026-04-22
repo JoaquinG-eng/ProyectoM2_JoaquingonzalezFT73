7509a5d6b6171d41790d71c97e61e82aae68cbdf
Joaquin <joamengancho@gmail.com>
2026-04-22 15:50:20 -0300

    configuración de variables de entorno con DATABASE_URL y limpieza de logs

--- src/db/index.js
+++ src/db/index.js
@@ -4,10 +4,7 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-
-let pool = new Pool({
+const pool = new Pool({
 connectionString: process.env.DATABASE_URL,
 });
 
