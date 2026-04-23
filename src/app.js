4c2ed2eafa0a81ac4bfad37bb14f808c474b52e5
Joaquin <joamengancho@gmail.com>
2026-04-23 13:18:18 -0300

    Agrega endpoint raíz (/) con información de la API y acceso a endpoints y documentación Swagger

--- src/app.js
+++ src/app.js
@@ -6,18 +6,30 @@ import authorsRoutes from "./routes/Authors.Routes.js";
 import postsRoutes from "./routes/Posts.Routes.js";
 import { errorHandler } from "./middlewares/Error.Middlewares.js";
 import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
 app.use("/api/authors", authorsRoutes);
 app.use("/api/posts", postsRoutes);
 
-
 const swaggerDocument = YAML.load("src/yaml/swagger.yaml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.get("/", (req, res) => {
+res.json({
+status: "200",
+endpoints: {
+authors: "/api/authors",
+posts: "/api/posts",
+docs: "/api-docs"
+}
+});
+});
+
 
 
 app.use(errorHandler);
